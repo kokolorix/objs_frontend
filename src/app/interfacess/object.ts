344faff0103b4a8127be3dc04d0fc3de7b5d7aab
Simon Reber <sr.dev@outlook.com>
2020-10-29 08:03:26 +0000

    Versuch mit Datenstruktur

--- src/app/interfacess/object.ts
+++ src/app/interfacess/object.ts
@@ -2,7 +2,7 @@
 
 
 export interface Value {
-    value:object;
+    // value:object;
 }
 
 export interface Property {
--- src/app/services/object.service.ts
+++ src/app/services/object.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ObjectRef } from '../interfacess/object';
+import { type } from 'os';
+import { Object, ObjectRef } from '../interfacess/object';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +21,19 @@ export class ObjectService {
       children:[]
     };
   }
+}
+
+const objectType : Object = {
+  id:'b06997d3-ae5c-4393-abec-5136254139b1',
+   type: null,
+   properties: [
+    {name:'Name',value:'ObjectType'}
+   ]
+}
+const TYPES: Object[] = [
+  {id:'b0a92cf9-d761-4c35-a78c-05ca70e706e9', type: objectType ,  properties:[{name:'Name',value:''}]}
+  
+];
+function generateObject() :Object{
+  return null;
 }
